0969b427d6003d3adf0d70d6622f7c6e6a4bf299
Mohamed Hassan <123888373+Mohamed-Hassan2041995@users.noreply.github.com>
2024-06-17 13:29:43 +0300

    add projects in my port

--- app/components/main/Projects.tsx
+++ app/components/main/Projects.tsx
@@ -3,25 +3,31 @@ import ProjectCard from "../sup/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-20 z-[20]" id="projects">
+    <div
+      className="flex flex-col items-center justify-center py-20 z-[20]"
+      id="projects"
+    >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 ">
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 ">
         <ProjectCard
-          src="/NextWebsite.png"
-          title=" Modern Next.js portfolio"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium incidunt, obcaecati velit suscipit id?"
+          src="/shopsy.png"
+          title=" Shopsy - Your perfect destination for online shopping"
+          description="Shopsy is a modern and easy-to-use e-commerce platform that provides you with the best deals on a wide range of products. If you're looking for the latest fashion trends, Shopsy has it all."
+          url="https://shopsy-market.netlify.app"
+        />
+        <ProjectCard
+          src="/task-manager.png"
+          title=" Task Manager - Streamline Your Workflow"
+          description="Task Manager is an intuitive and powerful tool designed to help you manage your tasks and projects efficiently. Whether you're working individually or as part of a team"
+          url="/https://manger-your-taske.netlify.app/"
         />
         <ProjectCard
           src="/NextWebsite.png"
           title=" Modern Next.js portfolio"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium incidunt, obcaecati velit suscipit id?"
-        />
-        <ProjectCard
-          src="/NextWebsite.png"
-          title=" Modern Next.js portfolio"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium incidunt, obcaecati velit suscipit id?"
+          url="/#"
         />
       </div>
     </div>
--- app/components/sup/ProjectCard.tsx
+++ app/components/sup/ProjectCard.tsx
@@ -5,21 +5,24 @@ interface Props {
   src: string;
   title: string;
   description: string;
+  url: string;
 }
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, url }: Props) => {
   return (
     <div className=" relative overflow-hidden rounded-lg shadow-lg border border-[#2a0e61]">
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
-      <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white"> {title}</h1>
-        <p className="mt-2 text-gray-300"> {description}</p>
-      </div>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <Image
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
+        <div className="relative p-4">
+          <h1 className="text-2xl font-semibold text-white"> {title}</h1>
+          <p className="mt-2 text-gray-300"> {description}</p>
+        </div>
+      </a>
     </div>
   );
 };
